07f180cb6f075f00008806b7fa2c4e36c0043341
eskalamarketing <plataformas@eskalamarketing.com>
2026-03-01 01:26:22 -0400

    Change response emoji from rocket to check mark

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Eskala Backend Hub activo 🚀");
+  res.send("Eskala Backend Hub activo ✅");
 });
 
 app.get("/meta-sync", (req, res) => {
